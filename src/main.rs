ce446bbc28f4c9d47df2cd39965c41979e7a5309
Sayama0310 <montblanc3060@gmail.com>
2023-06-03 13:29:25 +0900

    feat: Create interface for matches_regex_in_file method
    This method takes a regular expression and a file name as input and verifies whether each line in the file matches the provided regular expression.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+pub type DynError = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+fn main() -> Result<(), DynError> {
+    matches_regex_in_file(".*", "example.txt")?;
+    Ok(())
 }
+
+/// Determines whether the given regular expression matches any line in the file.
+///
+/// # Arguments
+///
+/// * `regex`: A string representing the regular expression pattern to match.
+/// * `file_name`: The name of the file to process.
+///
+/// # Returns
+///
+/// * `Ok(())` if the regular expression matches at least one line in the file.
+/// * `Err(DynError)` if the file does not exist or is not accessible.
+///
+/// Note: This method assumes that the file exists and is accessible.
+fn matches_regex_in_file(regex: &str, file_name: &str) -> Result<(), DynError> {
+    // Implementation goes here
+    Ok(())
+}
+
