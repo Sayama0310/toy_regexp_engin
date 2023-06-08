699a777fd6b6a022e6a3b49e4c1e5ef2509ad05d
Sayama0310 <montblanc3060@gmail.com>
2023-06-09 03:04:00 +0900

    document: Define the syntax of regular expressions in BNF.
    Adopt Perl-style regular expressions. However, it is undecided whether to implement all features.

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -1,6 +1,30 @@
 //! Parser Module
 //!
 //! This module contains the implementation of the regular expression parser.
+//!
+//! Syntax of Regular Expressions with BNF
+//! [Perl Style Regular Expressions](https://www2.cs.sfu.ca/~cameron/Teaching/384/99-3/regexp-plg.html)
+//! --------------------------------------
+//! ```text
+//! <regexp>            ::= <union> | <simple-regexp>
+//! <union>             ::= <simple-regexp> '|' <regexp>
+//! <simple-regexp>     ::= <concatenation> | <basic-regexp>
+//! <concatenation>     ::= <basic-regexp> <simple-regexp>
+//! <basic-regexp>      ::= <star> | <plus> | <elementary-regexp>
+//! <star>              ::= <elementary-regexp> '*'
+//! <plus>              ::= <elementary-regexp> '+'
+//! <elementary-regexp> ::= <group> | <any> | <eos> | <char> | <set>
+//! <group>             ::= '(' <regexp> ')'
+//! <any>               ::= '.'
+//! <eos>               ::= '$'
+//! <char>              ::= any non metacharacter | '\' metacharacter
+//! <set>               ::= <positive-set> | <negative-set>
+//! <positive-set>      ::= '[' <set-items> ']'
+//! <negative-set>      ::= '[^' <set-items> ']'
+//! <set-items>         ::= { <set-item> }
+//! <set-item>          ::= <range> | <char>
+//! <range>             ::= <char> '-' <char>
+//! ```
 pub fn parse(regexp: &str) -> Result<AST, ParseError> {
     let mut parser = Parser::new(regexp);
     parser.parse()
